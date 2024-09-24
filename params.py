0418a297e045bbeb6f786f6a33a9f0055a187ee2
bugdaryan <bugdaryan@gmail.com>
2024-09-24 22:13:03 +0000

    Code cleanup

--- params.py
+++ params.py
@@ -9,14 +9,15 @@ config_model = config['model']
 config_tokenizer = config['tokenizer']
 
 metric_greater_is_better_map = {
-    "bleu": True,
-    "bertscore": True,
-    "meteor": True,
-    "rouge": True,
-    "loss": False
+    'bleu': True,
+    'bertscore': True,
+    'meteor': True,
+    'rouge': True,
+    'loss': False
 }
 
 metric_greater_is_better = metric_greater_is_better_map[config_train['metric_for_best_model']]
 
 en_token = config_tokenizer['tokens']['en']
 ru_token = config_tokenizer['tokens']['ru']
+device = config['device']
