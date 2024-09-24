c2254ff8ec9a783f0f94bb227d455bba9d9e10af
bugdaryan <bugdaryan@gmail.com>
2024-09-24 22:13:22 +0000

    Update inference code

--- inference.py
+++ inference.py
@@ -1,63 +1,41 @@
-import yaml
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import wandb
-import json
-from safetensors.torch import load_file
-import re
+from params import en_token, ru_token, config_model, device
+from utils import load_model_tokenizer, postprocess_text
 
-
-with open('config.yml', 'r') as f:
-    config = yaml.safe_load(f)
-
-batch_size = int(config['training']['batch_size'])
-epochs = int(config['training']['epochs'])
-learning_rate = float(config['training']['learning_rate'])
-warmup_steps = int(config['training']['warmup_steps'])
-gradient_accumulation_steps = int(config['training']['gradient_accumulation_steps'])
-max_grad_norm = float(config['training']['max_grad_norm'])
-max_length = int(config['model']['max_length'])
-
-
+def inference(input_text):
+    model, tokenizer = load_model_tokenizer(config_model['model_name'], en_token, ru_token, device)
+    translated_text = translate(input_text, model, tokenizer, config_model['max_length'])
+    return translated_text
 
 def translate(text, model, tokenizer, max_length):
     model.eval()
-    input_text = f"[EN] {text} [RU]"
-    inputs = tokenizer(input_text, return_tensors="pt", truncation=True, max_length=max_length, padding=True)
+    input_text = f'{en_token} {text} {ru_token}'
+    inputs = tokenizer(input_text, return_tensors='pt', truncation=True, max_length=max_length, padding=True)
     inputs = {key: value.to(model.device) for key, value in inputs.items()}
     with torch.no_grad():
         outputs = model.generate(**inputs, max_length=max_length, num_beams=5, early_stopping=True, pad_token_id=tokenizer.pad_token_id)
     translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    translation = translated_text.replace(text, "").strip()
-    words = re.findall(r'\w+|[^\w\s]', translation, re.UNICODE)
-    processed_words = []
-
-    for i in range(len(words)):
-        if i > 0 and words[i] == words[i-1]:
-            break
-        if words[i] not in ['?', '!', '.', ','] and len(words[i]) < 20:
-            processed_words.append(words[i])
-    return translated_text, ' '.join(processed_words)
+    translation = translated_text.replace(text, '').strip()
+    processed_text = postprocess_text(translation)
+    return processed_text
 
 
-en_token = "[EN]"
-ru_token = "[RU]"
-load_safetensors = False
-if load_safetensors:
-    tokenizer = AutoTokenizer.from_pretrained(config['model']['name'])
-    model = AutoModelForCausalLM.from_pretrained(config['model']['name'], device_map="auto")
-    tokenizer.add_special_tokens({'additional_special_tokens': [en_token, ru_token], 'pad_token': '[PAD]'})
-    model.resize_token_embeddings(len(tokenizer))
+# load_safetensors = False
+# if load_safetensors:
+#     tokenizer = AutoTokenizer.from_pretrained(config_model['name'])
+#     model = AutoModelForCausalLM.from_pretrained(config_model['name'], device_map='auto')
+#     tokenizer.add_special_tokens({'additional_special_tokens': [en_token, ru_token], 'pad_token': '[PAD]'})
+#     model.resize_token_embeddings(len(tokenizer))
 
-    state = load_file("./results/TinyLlama_v1.1/checkpoint-200/model.safetensors")
-    model.load_state_dict(state)
-else:
-    tokenizer = AutoTokenizer.from_pretrained('./results/TinyLlama_v1.1')
-    model = AutoModelForCausalLM.from_pretrained('./results/TinyLlama_v1.1', device_map="auto")
+#     state = load_file('./results/TinyLlama_v1.1/checkpoint-200/model.safetensors')
+#     model.load_state_dict(state)
+# else:
+#     tokenizer = AutoTokenizer.from_pretrained('./results/TinyLlama_v1.1')
+#     model = AutoModelForCausalLM.from_pretrained('./results/TinyLlama_v1.1', device_map='auto')
 
-input_text = "I am eatting apple while i am working at Apple"
-full_translation, translated_text = translate(input_text, model, tokenizer, max_length)
-print(f"Original text: {input_text}")
-print(f"Translated text: {translated_text}")
-print(f"Full translation: {full_translation}")
+# input_text = 'I am eatting apple while i am working at Apple'
+# full_translation, translated_text = translate(input_text, model, tokenizer, max_length)
+# print(f'Original text: {input_text}')
+# print(f'Translated text: {translated_text}')
+# print(f'Full translation: {full_translation}')
