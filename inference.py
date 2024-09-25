c902da74e9956f1a2f4536adb023f1d60f47a909
bugdaryan <bugdaryan@gmail.com>
2024-09-25 18:16:30 +0000

    Update inference script for seq2seq

--- inference.py
+++ inference.py
@@ -1,10 +1,10 @@
 import torch
-from params import en_token, ru_token, config_model, device
-from utils import load_model_tokenizer, postprocess_text, load_ner_model
+from params import config_model, device
+from utils import load_model_tokenizer, load_ner_model
 from flair.data import Sentence
 
 def inference(input_text, translation_mode='normal'):
-    model, tokenizer = load_model_tokenizer(config_model['model_name'], en_token, ru_token, device)
+    model, tokenizer = load_model_tokenizer(config_model['model_name'], config_model['tokenizer_name'], device)
     
     if translation_mode == 'undertranslation':
         input_text = undertranslate(input_text)
@@ -19,16 +19,12 @@ def inference(input_text, translation_mode='normal'):
 
 def translate(text, model, tokenizer, max_length):
     model.eval()
-    input_text = f'{en_token} {text} {ru_token}'
-    inputs = tokenizer(input_text, return_tensors='pt', truncation=True, max_length=max_length, padding=True)
+    inputs = tokenizer(text, return_tensors='pt', max_length=max_length, truncation=True, padding=True)
     inputs = {key: value.to(model.device) for key, value in inputs.items()}
     with torch.no_grad():
-        outputs = model.generate(**inputs, max_length=max_length, num_beams=5, early_stopping=True, pad_token_id=tokenizer.pad_token_id)
+        outputs = model.generate(**inputs, max_length=max_length, num_beams=4, early_stopping=True)
     translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-    translation = translated_text.replace(text, '').strip()
-    processed_text = postprocess_text(translation)
-    return processed_text
+    return translated_text
 
 
 def undertranslate(original_text):
