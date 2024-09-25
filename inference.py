423e1724010705f1b00939f6c058be840f1b017c
bugdaryan <bugdaryan@gmail.com>
2024-09-25 13:03:53 +0000

    Add checkpoint loading

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import torch
 from params import en_token, ru_token, config_model, device
 from utils import load_model_tokenizer, postprocess_text
 
-def inference(input_text):
+def inference(input_text, translation_mode='normal'):
     model, tokenizer = load_model_tokenizer(config_model['model_name'], en_token, ru_token, device)
     translated_text = translate(input_text, model, tokenizer, config_model['max_length'])
     return translated_text
@@ -20,22 +20,8 @@ def translate(text, model, tokenizer, max_length):
     processed_text = postprocess_text(translation)
     return processed_text
 
+def undertranslate(text, model, tokenizer, max_length):
+    pass
 
-# load_safetensors = False
-# if load_safetensors:
-#     tokenizer = AutoTokenizer.from_pretrained(config_model['name'])
-#     model = AutoModelForCausalLM.from_pretrained(config_model['name'], device_map='auto')
-#     tokenizer.add_special_tokens({'additional_special_tokens': [en_token, ru_token], 'pad_token': '[PAD]'})
-#     model.resize_token_embeddings(len(tokenizer))
-
-#     state = load_file('./results/TinyLlama_v1.1/checkpoint-200/model.safetensors')
-#     model.load_state_dict(state)
-# else:
-#     tokenizer = AutoTokenizer.from_pretrained('./results/TinyLlama_v1.1')
-#     model = AutoModelForCausalLM.from_pretrained('./results/TinyLlama_v1.1', device_map='auto')
-
-# input_text = 'I am eatting apple while i am working at Apple'
-# full_translation, translated_text = translate(input_text, model, tokenizer, max_length)
-# print(f'Original text: {input_text}')
-# print(f'Translated text: {translated_text}')
-# print(f'Full translation: {full_translation}')
+def overtranslate(text, model, tokenizer, max_length):
+    pass
